e1089db1a029152d6e4614d7d286b28ee0559fe0
Michael Tharpe <mtharpe@users.noreply.github.com>
2022-10-11 16:28:02 -0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ if [ -z "$ansible" ]; then
  sudo dnf install ansible -y
 fi
 
-until ansible-playbook --extra-vars "local_user=${USER}" site.yml; do
+until ansible-playbook --extra-vars "local_user=${USER}" setup_workstation.yml; do
   echo Ansible run disrupted, retrying in 10 seconds...
   sleep 10
 done
